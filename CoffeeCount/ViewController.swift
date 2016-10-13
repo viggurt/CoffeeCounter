281cbe10842b01c7620b03ecef38ebdf482d4082
viggurt <viktor.olsson@live.com>
2016-10-13 09:40:57 +0200

    First Commit

--- CoffeeCount/ViewController.swift
+++ CoffeeCount/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var teaButton: UIButton!
+    @IBOutlet weak var coffeeButton: UIButton!
+    @IBOutlet weak var coffeeCreator: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        teaButton.layer.cornerRadius = self.teaButton.frame.height/2
+        teaButton.clipsToBounds = true
+        
+        coffeeButton.layer.cornerRadius = self.teaButton.frame.height/2
+        coffeeButton.clipsToBounds = true
+        coffeeCreator.layer.cornerRadius = self.teaButton.frame.height/2
+        coffeeCreator.clipsToBounds = true
+
     }
 
     override func didReceiveMemoryWarning() {
